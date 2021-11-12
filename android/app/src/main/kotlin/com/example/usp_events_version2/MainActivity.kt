95662e2a9832caca54bb6232ffac64345dc0b1ef
Fazeel-Ali <S11157285@student.usp.ac.fj>
2021-11-12 20:20:55 +1200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/usp_events_version2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.usp_events_version2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:usp_events_version2/view/login.dart';
+//import 'package:usp_events_app/view/login.dart';
+//import 'package:usp_events_app/view/splashScreen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Email And Password Login',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
